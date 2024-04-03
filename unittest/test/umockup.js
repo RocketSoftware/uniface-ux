225fb1b9493248c297bc5d7a287beaeb956543f3
Sjaak van Schie <jvanschie@rocketsoftware.com>
2024-04-03 16:20:40 +0200

    Add _uf namespace for uconsole

--- unittest/test/umockup.js
+++ unittest/test/umockup.js
@@ -29,4 +29,57 @@
     global.uniface = {
         RESET: {}
     };
+
+    /**
+     * Mockup of the relevant part of private _uf namespace
+     *
+     * @name _uf
+     * @namespace
+     * @private
+     */
+    global._uf = {
+        uconsole : (function() {
+            var _log, _error, _warn;
+
+            if ((typeof console === 'undefined') || (typeof console.log === 'undefined')) {
+                _log = function(_aMessage) {
+                    // This empty function is in case the browser does not support console logging.
+                };
+                _error = _log;
+                _warn = _log;
+            } else {
+                /* console.log */
+                _log = function(aMessage) {
+                    console.log(aMessage);
+                };
+                /* console.error */
+                if (typeof console.error === 'undefined') {
+                    _error = function(aMessage) {
+                        _log("Error: " + aMessage);
+                    };
+                } else {
+                    _error = function(aMessage) {
+                        console.error(aMessage);
+                    };
+                }
+                /* console.warn */
+                if (typeof console.warn === 'undefined') {
+                    _error = function(aMessage) {
+                        _log("Warning: " + aMessage);
+                    };
+                } else {
+                    _warn = function(aMessage) {
+                        console.warn(aMessage);
+                    };
+                }
+            }
+
+            return {
+                log : _log,
+                error : _error,
+                warn : _warn
+            };
+        })()
+    };
+
 })(this);
