289eaa6866539277c95f9fe40fd095521dd76a3e
Sjaak van Schie <jvanschie@rocketsoftware.com>
2024-08-28 16:17:35 +0200

    Merge updates to refactored sources from harness. Commit 78e3e2 on Aug 26, Merge branch 'feature/UNI-36662-refactor-uxnumberfield-minmax' into 'feature/UXWF'

--- sources/ux/loader_UXWF.js
+++ sources/ux/loader_UXWF.js
@@ -1,5 +1,6 @@
 // @ts-check
 import "https://unpkg.com/@fluentui/web-components";
+
 import "./button_UXWF.js";
 import "./checkbox_UXWF.js";
 import "./number_field_UXWF.js";
@@ -7,11 +8,9 @@ import "./plain_text_UXWF.js";
 import "./radio_group_UXWF.js";
 import "./switch_UXWF.js";
 import "./text_field_UXWF.js";
+
 import "./controlbar_UXWF.js";
-import "./switch_UXWF.js";
 
-
-// import "./controlbar_UXWF.js";
 // import "./data_grid_UXWF.js";
 
 /**
