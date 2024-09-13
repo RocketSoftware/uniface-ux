2264b3a1499706622959e3b603cc7503ebf64349
Sjaak van Schie <jvanschie@rocketsoftware.com>
2024-09-13 14:42:37 +0200

    Webpack for JS & CSS bundles

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path');
+module.exports = {
+  plugins: [new MiniCssExtractPlugin(
+    {filename:"unifaceux.min.css"}
+  )],
+  mode: 'none',
+  // other configuration options...
+  devtool: "source-map",
+  context:path.resolve(__dirname, 'sources','ux'),
+  entry: ["./loader.js", "./loader.css"],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "unifaceux.min.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  }
+};
