bf1093e7e02bccc7b1f72f5f8a518951264c61d7
Sjaak van Schie <jvanschie@rocketsoftware.com>
2024-10-30 15:49:18 +0100

    Add testing PlainText widget

--- testAutomation/tests/UXWidgetTest.spec.js
+++ testAutomation/tests/UXWidgetTest.spec.js
@@ -19,67 +19,73 @@ test.describe('Widget Tests', () => {
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('Button Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.buttonWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('Checkbox Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.checkboxWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
-    test('Number Widget Tests', async ({ page }) => {
+
+    test('NumberField Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.numberFieldWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
+    test('PlainText Widget Tests', async ({ page }) => {
+        const newPage = await indexPage.openNewPage(indexPage.plainTextWidgetLink);
+        const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
+        await homePage.checkFailuresEncountered(newPage, test);
+    });
+
     test('RadioGroup Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.radioGroupWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('Select Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.selectWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('Switch Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.switchWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('TextArea Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.textAreaWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('TextField Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.textFieldWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('Base Class Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.baseWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
-    test('WidgetClass Widget Tests', async ({ page }) => {
+
+    test('Widget Class Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.widgetClassWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
         await homePage.checkFailuresEncountered(newPage, test);
     });
-    
+
     test('Workers Class Widget Tests', async ({ page }) => {
         const newPage = await indexPage.openNewPage(indexPage.workersClassWidgetLink);
         const extractedText = await homePage.checkElementVisibilityAndExtractText(newPage, homePage.resultsFooterText);
