15accb973d20aaf773635f37897e51b857461b3f
Ashik Moosa <aka@rocketsoftware.com>
2025-01-24 19:34:25 +0530

    [UNI-39511] Fixed delegated property name in tests

--- test/ux/test_ux_base.js
+++ test/ux/test_ux_base.js
@@ -19,7 +19,7 @@ import { Widget } from "../../src/ux/widget.js";
 
   describe("test Base Class methods", function () {
 
-    let base, widgetClass, propId, subWidgetId, subWidgetClass, subWidgetStyleClass, subWidgetTriggers, delegatedProperties, defaultValue, triggerName,
+    let base, widgetClass, propId, subWidgetId, subWidgetClass, subWidgetStyleClass, subWidgetTriggers, subWidgetDelegatedProperties, defaultValue, triggerName,
       url, functionName, message, consequence, consoleLogSpy, worker;
 
     beforeEach(function () {
@@ -76,15 +76,15 @@ import { Widget } from "../../src/ux/widget.js";
       subWidgetStyleClass = "u-change-button";
       subWidgetClass = new Button();
       subWidgetTriggers = ["trigger1", "trigger2"];
-      delegatedProperties = ["html:disabled"];
+      subWidgetDelegatedProperties = ["html:disabled"];
 
-      base.registerSubWidget(widgetClass, subWidgetId, subWidgetClass, subWidgetStyleClass, subWidgetTriggers, delegatedProperties);
+      base.registerSubWidget(widgetClass, subWidgetId, subWidgetClass, subWidgetStyleClass, subWidgetTriggers, subWidgetDelegatedProperties);
 
       expect(String(Object.keys(widgetClass.subWidgets))).to.equal(subWidgetId);
       expect((Object.keys(widgetClass.subWidgets[subWidgetId]))).to.have.lengthOf(4);
       expect(widgetClass.subWidgets[subWidgetId].styleClass).to.equal(subWidgetStyleClass);
       expect(widgetClass.subWidgets[subWidgetId].triggers).to.equal(subWidgetTriggers);
-      expect(widgetClass.subWidgets[subWidgetId].delegatedProperties).to.equal(delegatedProperties);
+      expect(widgetClass.subWidgets[subWidgetId].delegatedProperties).to.equal(subWidgetDelegatedProperties);
     });
 
     it("registerSubWidgetWorker", function () {
