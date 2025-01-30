43b8417a2bfe0a91b02ee9035731dca69fea7316
Sjaak van Schie <jvanschie@rocketsoftware.com>
2025-01-30 14:06:49 +0100

    UNI-42180 Document functions

--- src/ux/dsp_connector.js
+++ src/ux/dsp_connector.js
@@ -1,8 +1,20 @@
-/* global UNIFACE */
+// @ts-check
+import { Widget } from "./widget.js"; // eslint-disable-line no-unused-vars
+
+/**
+ * Register a UX Widget class with a name. For Uniface DSP, the same name needs to be used in the web.ini configuration.
+ * @param {String} widgetName - the name to register with.
+ * @param {typeof Widget} widgetClass - Specifies the Widget class to register.
+ */
 export function registerWidgetClass(widgetName, widgetClass) {
-  UNIFACE.ClassRegistry.add(widgetName, widgetClass);
+  globalThis.UNIFACE.ClassRegistry.add(widgetName, widgetClass);
 }
 
+/**
+ * Get the UX Widget class from the Uniface registry.
+ * @param {String} widgetName - the name of the UX widget to get.
+ * @return {typeof Widget}
+ */
 export function getWidgetClass(widgetName) {
-  return UNIFACE.ClassRegistry.get(widgetName);
+  return globalThis.UNIFACE.ClassRegistry.get(widgetName);
 }
