e973f61525cf2ab16736ab4062da2084b78a99c1
Ming Zhu <mzhu@rocketsoftware.com>
2025-01-31 16:33:43 +0100

    UNI-42180 remove the dependence of widget.js from dsp_connector

--- src/ux/dsp_connector.js
+++ src/ux/dsp_connector.js
@@ -1,10 +1,9 @@
 // @ts-check
-import { Widget } from "./widget.js"; // eslint-disable-line no-unused-vars
 
 /**
  * Register a UX Widget class with a name. For Uniface DSP, the same name needs to be used in the web.ini configuration.
  * @param {String} widgetName - the name to register with.
- * @param {typeof Widget} widgetClass - Specifies the Widget class to register.
+ * @param {Object} widgetClass - Specifies the Widget class to register.
  */
 export function registerWidgetClass(widgetName, widgetClass) {
   globalThis.UNIFACE.ClassRegistry.add(widgetName, widgetClass);
@@ -13,7 +12,7 @@ export function registerWidgetClass(widgetName, widgetClass) {
 /**
  * Get the UX Widget class from the Uniface registry.
  * @param {String} widgetName - the name of the UX widget to get.
- * @return {typeof Widget}
+ * @return {Object | undefined} the UX Widget class.
  */
 export function getWidgetClass(widgetName) {
   return globalThis.UNIFACE.ClassRegistry.get(widgetName);
