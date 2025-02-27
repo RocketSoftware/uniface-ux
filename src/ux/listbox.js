edcd117e0e2660e5a6f8c2ab5f1f933cde85308a
Nishant Kumar <nkumar5@rocketsoftware.com>
2025-02-27 12:52:59 +0530

    UNI-43219 Resolve merge conflicts

--- src/ux/listbox.js
+++ src/ux/listbox.js
@@ -115,6 +115,44 @@ export class Listbox extends Widget {
   };
 
   /**
+   * Private Worker: ListBoxValRep
+   * This is specialized worker to accommodate Listbox with no valrep defined.
+   * @class ListBoxValRep
+   * @extends {SlottedElementsByValRep}
+   */
+  static ListBoxValRep = class extends SlottedElementsByValRep {
+
+    /**
+     * Creates an instance of ListBoxValRep.
+     * @param {typeof Widget} widgetClass
+     * @param {String} tagName
+     * @param {String} styleClass
+     * @param {String} elementQuerySelector
+     */
+    constructor(widgetClass, tagName, styleClass, elementQuerySelector) {
+      super(widgetClass, tagName, styleClass, elementQuerySelector);
+      this.registerSetter(widgetClass, "layout", this);
+      this.registerSetter(widgetClass, "valrep", this);
+    }
+
+    refresh(widgetInstance) {
+      const valrep = this.getNode(widgetInstance.data, "valrep");
+      const value = this.getNode(widgetInstance.data, "value");
+      let matchedValrepObj = valrep ? valrep.find((valrepObj) => valrepObj.value === value) : undefined;
+      if (valrep.length > 0) {
+        if (matchedValrepObj) {
+          widgetInstance.elements.widget.valrepUpdated = true;
+        }
+        super.refresh(widgetInstance);
+      } else {
+        const ListBoxElement = this.getElement(widgetInstance);
+        this.removeValRepElements(widgetInstance);
+        ListBoxElement.appendChild(document.createElement(this.tagName));
+      }
+    }
+  };
+
+  /**
    * Widget definition.
    */
   // prettier-ignore
@@ -131,7 +169,7 @@ export class Listbox extends Widget {
     new HtmlAttributeBoolean(this, "html:readonly", "readonly", false, true),
     new HtmlAttributeBoolean(this, "html:hidden", "hidden", false),
     new HtmlAttributeNumber(this, "html:tabindex", "tabIndex", -1, null, 0),
-    new SlottedElementsByValRep(this, "fluent-option", "u-option", ""),
+    new this.ListBoxValRep(this, "fluent-option", "u-option", ""),
     new this.ListboxSelectedValue(this, "value", ""),
     new IgnoreProperty(this, "html:minlength"),
     new IgnoreProperty(this, "html:maxlength")
