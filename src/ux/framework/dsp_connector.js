35fda252ea9a28b1eccb540b053944b1230ddbbc
Sjaak van Schie <jvanschie@rocketsoftware.com>
2025-04-08 16:28:03 +0200

    UNI-40946 Change import when only used in JSDoc

--- src/ux/framework/dsp_connector.js
+++ src/ux/framework/dsp_connector.js
@@ -3,7 +3,7 @@
 /**
  * Register a UX Widget class with a name. For Uniface DSP, the same name needs to be used in the web.ini configuration.
  * @param {String} widgetName - the name to register with.
- * @param {Object} widgetClass - Specifies the Widget class to register.
+ * @param {typeof import("./widget.js").Widget} widgetClass - Specifies the Widget class to register.
  */
 export function registerWidgetClass(widgetName, widgetClass) {
   globalThis.UNIFACE.ClassRegistry.add(widgetName, widgetClass);
@@ -12,7 +12,7 @@ export function registerWidgetClass(widgetName, widgetClass) {
 /**
  * Get the UX Widget class from the Uniface registry.
  * @param {String} widgetName - the name of the UX widget to get.
- * @return {Object | undefined} the UX Widget class.
+ * @return {typeof import("./widget.js").Widget | undefined} the UX Widget class.
  */
 export function getWidgetClass(widgetName) {
   return globalThis.UNIFACE.ClassRegistry.get(widgetName);
