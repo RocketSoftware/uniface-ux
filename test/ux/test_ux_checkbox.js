7e7f82dc8195e5fe144f7c9acc671d8d9b7c9fdc
Jeremy Lesser <jlesser@rocketsoftware.com>
2025-04-28 10:47:38 -0600

    Changed mapTrigger for checkbox

--- test/ux/test_ux_checkbox.js
+++ test/ux/test_ux_checkbox.js
@@ -115,15 +115,24 @@
 
   describe("mapTrigger()", function () {
     const element = tester.processLayout();
-    const widget = tester.onConnect();
+    const testData = {
+      "onchange" : "valuechange"
+    };
+    let widget;
 
-    it("verify mapTrigger() and onchange event", function () {
-      widget.mapTrigger("click");
-      const event = new window.Event("click");
-      element.dispatchEvent(event);
-      assert(widget.elements.widget === element, "Widget is not connected.");
+    beforeEach(function () {
+      widget = tester.onConnect();
     });
 
+    Object.keys(testData).forEach((triggerName) => {
+      it(`Test mapping of trigger '${triggerName}'`, function () {
+        const triggerMapping = widget.mapTrigger(triggerName);
+        assert(triggerMapping, `Trigger '${triggerName}' is not mapped!`);
+        assert(triggerMapping.element === tester.element, `Trigger '${triggerName}' is not mapped to correct HTMLElement!`);
+        assert(triggerMapping.event_name === testData[triggerName],
+          `trigger '${triggerName}' should be mapped to event '${testData[triggerName]}', but got '${triggerMapping.event_name}'!`);
+      });
+    });
   });
 
   describe("Checkbox onchange event", function () {
