c4de0bc7f98c618f4d5b06dfad647317f5a89d4e
Rohan Naidu <rnaidu@rocketsoftware.com>
2025-05-14 14:01:21 +0530

    UNI-45471: Changes for base.js

--- src/ux/framework/base.js
+++ src/ux/framework/base.js
@@ -24,7 +24,6 @@ export class Base {
    * @param {Worker} worker - Specifies the worker.
    */
   registerSetter(widgetClass, propId, worker) {
-    // widgetClass.setters[propId] ??= [];
     widgetClass.setters[propId] = widgetClass.setters[propId] || [];
     widgetClass.setters[propId].push(worker);
   }
